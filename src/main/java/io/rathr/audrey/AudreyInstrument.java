a05ca5f36bae863316f84341bbcd2bf8bd3ddae6
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-11-29 14:00:22 +0100

    Add two execution event nodes to set enter root flag

--- src/main/java/io/rathr/audrey/AudreyInstrument.java
+++ src/main/java/io/rathr/audrey/AudreyInstrument.java
@@ -6,13 +6,15 @@ import com.oracle.truffle.api.frame.FrameSlot;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.instrumentation.*;
 import com.oracle.truffle.api.instrumentation.TruffleInstrument.Registration;
-import com.oracle.truffle.api.interop.*;
+import com.oracle.truffle.api.interop.ForeignAccess;
+import com.oracle.truffle.api.interop.Message;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.nodes.LanguageInfo;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.RootNode;
 import com.oracle.truffle.api.source.SourceSection;
 import org.graalvm.options.OptionDescriptors;
-import org.graalvm.polyglot.Context;
 
 import java.util.Arrays;
 import java.util.List;
@@ -27,6 +29,7 @@ public final class AudreyInstrument extends TruffleInstrument {
 
     public static final String ID = "audrey";
     private static final Class CALL_TAG = StandardTags.CallTag.class;
+    private static final Class STATEMENT_TAG = StandardTags.StatementTag.class;
     private static final Class ROOT_TAG = StandardTags.RootTag.class;
 
     private Map<SourceSection, Set<Sample>> sampleMap = new ConcurrentHashMap<>();
@@ -44,15 +47,27 @@ public final class AudreyInstrument extends TruffleInstrument {
         }
     }
 
+    class InstrumentationContext {
+        private boolean enteringRoot = false;
+
+        public boolean isEnteringRoot() {
+            return enteringRoot;
+        }
+
+        public void setEnteringRoot(final boolean enteringRoot) {
+            this.enteringRoot = enteringRoot;
+        }
+    }
+
     @Override
     protected void onCreate(TruffleInstrument.Env env) {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            sampleMap.forEach((sourceSection, samples) -> {
-                System.out.println(sourceSection + ":");
-                samples.forEach(sample -> System.out.print(sample.value + ", "));
-                System.out.println();
-            });
-        }));
+//        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+//            sampleMap.forEach((sourceSection, samples) -> {
+//                System.out.println(sourceSection + ":");
+//                samples.forEach(sample -> System.out.print(sample.value + ", "));
+//                System.out.println();
+//            });
+//        }));
 
         if (!env.getOptions().get(AudreyCLI.ENABLED)) {
             return;
@@ -71,108 +86,122 @@ public final class AudreyInstrument extends TruffleInstrument {
             .build();
 
         final SourceSectionFilter.Builder builder = SourceSectionFilter.newBuilder();
-        final SourceSectionFilter filter = builder.sourceFilter(sourceFilter)
+        final SourceSectionFilter statementFilter = builder.sourceFilter(sourceFilter)
+            .tagIs(STATEMENT_TAG)
+            .build();
+
+        final SourceSectionFilter rootFilter = builder.sourceFilter(sourceFilter)
             .tagIs(ROOT_TAG)
             .build();
 
         Instrumenter instrumenter = env.getInstrumenter();
-        instrumenter.attachExecutionEventFactory(filter, context -> new ExecutionEventNode() {
+        final InstrumentationContext instrumentationContext = new InstrumentationContext();
 
+        instrumenter.attachExecutionEventFactory(rootFilter, context -> new ExecutionEventNode() {
             @Override
-            protected void onEnter(VirtualFrame frame) {
+            protected void onEnter(final VirtualFrame frame) {
+                handleOnEnter();
+            }
+
+            @TruffleBoundary
+            private void handleOnEnter() {
+                instrumentationContext.setEnteringRoot(true);
+            }
+        });
+
+        instrumenter.attachExecutionEventFactory(statementFilter, context -> new ExecutionEventNode() {
+            @Override
+            protected void onEnter(final VirtualFrame frame) {
                 handleOnEnter(frame);
             }
 
 
             @TruffleBoundary
-            private void handleOnEnter(VirtualFrame frame) {
+            private void handleOnEnter(final VirtualFrame frame) {
                 final FrameDescriptor descriptor = frame.getFrameDescriptor();
                 final Node instrumentedNode = context.getInstrumentedNode();
 
                 final SourceSection sourceSection = context.getInstrumentedSourceSection();
                 final String languageId = sourceSection.getSource().getLanguage();
 
-                if (descriptor.getSize() > 0) {
+                final Scope scope = env.findLocalScopes(instrumentedNode, frame).iterator().next();
+                final Object variables = scope.getVariables();
 
-                    System.out.println("Root node: " + extractRootName(instrumentedNode));
+                try {
+                    final TruffleObject keys = ForeignAccess.sendKeys(
+                        Message.KEYS.createNode(),
+                        (TruffleObject) variables
+                    );
 
-                    Iterable<Scope> scopes = env.findLocalScopes(instrumentedNode, frame);
+                    System.out.println("hi");
 
-                    for (Scope scope : scopes) {
-                        TruffleObject arguments = (TruffleObject) scope.getArguments();
-                        if (arguments == null) {
-                            continue;
-                        }
-
-                        try {
-                            boolean xExists = KeyInfo.isExisting(ForeignAccess.sendKeyInfo(Message.KEY_INFO.createNode(),
-                                arguments, "x"));
-
-                            System.out.println("Does x exist according to KeyInfo?: " + xExists);
-
-                            Object x = ForeignAccess.sendRead(Message.READ.createNode(), arguments, "x");
-                            Object y = ForeignAccess.sendRead(Message.READ.createNode(), arguments, "y");
-
-                            System.out.println("Argument x extracted from scope: " + getString(languageId, x));
-                            System.out.println("Argument y extracted from scope: " + getString(languageId, y));
-                        } catch (UnknownIdentifierException e) {
-                            e.printStackTrace();
-                        } catch (UnsupportedMessageException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    System.out.println("Source section: " + sourceSection);
-
-                    final List<? extends FrameSlot> slots = descriptor.getSlots();
-                    slots.forEach(slot -> {
-                        System.out.println("slot name: " + slot.getIdentifier());
-
-                        final Object value = frame.getValue(slot);
-                        final String string = getString(languageId, value);
-                        System.out.println("slot value: " + string);
-                    });
-
-                    final Object[] arguments = frame.getArguments();
-                    Arrays.asList(arguments).forEach(arg -> {
-                        final String string = getString(languageId, arg);
-                        System.out.println("arg: " + string);
-                    });
-
-
-                    System.out.println("\n");
-                }
-            }
-
-            @Override
-            protected void onReturnValue(VirtualFrame frame, Object result) {
-                handleOnReturnValue(frame, result);
-            }
-
-            @TruffleBoundary
-            private void handleOnReturnValue(VirtualFrame frame, Object result) {
-                if (result == null) {
-                    return;
+                } catch (UnsupportedMessageException e) {
+                    e.printStackTrace();
                 }
 
-                final SourceSection sourceSection = context.getInstrumentedSourceSection();
-                final String languageId = sourceSection.getSource().getLanguage();
 
-                final String value = getString(languageId, result);
-                final Object metaObject = env.findMetaObject(getLanguageInfo(languageId), result);
-
-                final Node instrumentedNode = context.getInstrumentedNode();
-
-                final Sample sample = new Sample(
-                    value,
-                    getString(languageId, metaObject),
-                    "return",
-                    extractRootName(instrumentedNode)
-                );
-
-                sampleMap.computeIfAbsent(sourceSection, section -> ConcurrentHashMap.newKeySet());
-                sampleMap.get(sourceSection).add(sample);
+//                if (descriptor.getSize() > 0) {
+//
+//                    System.out.println("Root node: " + extractRootName(instrumentedNode));
+//
+//                    Iterable<Scope> scopes = env.findLocalScopes(instrumentedNode, frame);
+//
+//                    for (Scope scope : scopes) {
+//                        final Object variables = scope.getVariables();
+//                        System.out.println("slkjfdslkjf");
+//                    }
+//
+//                    System.out.println("Source section: " + sourceSection);
+//
+//                    final List<? extends FrameSlot> slots = descriptor.getSlots();
+//                    slots.forEach(slot -> {
+//                        System.out.println("slot name: " + slot.getIdentifier());
+//
+//                        final Object value = frame.getValue(slot);
+//                        final String string = getString(languageId, value);
+//                        System.out.println("slot value: " + string);
+//                    });
+//
+//                    final Object[] arguments = frame.getArguments();
+//                    Arrays.asList(arguments).forEach(arg -> {
+//                        final String string = getString(languageId, arg);
+//                        System.out.println("arg: " + string);
+//                    });
+//
+//
+//                    System.out.println("\n");
+//                }
             }
+
+//            @Override
+//            protected void onReturnValue(VirtualFrame frame, Object result) {
+//                handleOnReturnValue(frame, result);
+//            }
+//
+//            @TruffleBoundary
+//            private void handleOnReturnValue(VirtualFrame frame, Object result) {
+//                if (result == null) {
+//                    return;
+//                }
+//
+//                final SourceSection sourceSection = context.getInstrumentedSourceSection();
+//                final String languageId = sourceSection.getSource().getLanguage();
+//
+//                final String value = getString(languageId, result);
+//                final Object metaObject = env.findMetaObject(getLanguageInfo(languageId), result);
+//
+//                final Node instrumentedNode = context.getInstrumentedNode();
+//
+//                final Sample sample = new Sample(
+//                    value,
+//                    getString(languageId, metaObject),
+//                    "return",
+//                    extractRootName(instrumentedNode)
+//                );
+//
+//                sampleMap.computeIfAbsent(sourceSection, section -> ConcurrentHashMap.newKeySet());
+//                sampleMap.get(sourceSection).add(sample);
+//            }
 
             /**
              * @return guest language string representation of object.

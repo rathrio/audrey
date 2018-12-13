ef7efb925766677eae18f2653342959a7375bcbf
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-12-13 14:47:18 +0100

    Add audrey server
    For communication with redis and unified api

---
+++ language_servers/audrey_server/lib/audrey_server.rb
@@ -0,0 +1,6 @@
+require "audrey_server/version"
+
+module AudreyServer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ language_servers/audrey_server/lib/audrey_server/version.rb
@@ -0,0 +1,3 @@
+module AudreyServer
+  VERSION = "0.1.0"
+end
